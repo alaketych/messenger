bd7930367b9ea83cf13662db6c3da7281da29fea
Alexandr Laketych <alaketych@ukr.net>
2018-06-01 22:13:32 +0300

    messenger mvvm

---
+++ VIEWnewHumberger/View/Entrance/Registration.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VIEWnewHumberger.View.Entrance
+{
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistationButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            List<SqlParameter> sqlParams = new List<SqlParameter>();
+
+            sqlParams.Add(new SqlParameter("Username", txtNewUserName.Text));
+            sqlParams.Add(new SqlParameter("Password", txtNewPassword.Text));
+
+            DAL.ExecSP("CreateUser", sqlParams);
+            MessageBox.Show("You have been registrated.");
+
+            LoginUser backLoginUser = new LoginUser();
+            backLoginUser.Show();
+            this.Close();
+        }
+    }
+}
---
+++ VIEWnewHumberger/View/MainMessenger/Dialogs.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VIEWnewHumberger.View.MainMessenger {
+    /// <summary>
+    /// Interaction logic for Dialogs.xaml
+    /// </summary>
+    public partial class Dialogs : UserControl {
+        public Dialogs()
+        {
+            InitializeComponent();
+        }
+
+        public ImageSource UserPhotos {
+            get { return UserPhoto.Source; }
+            set { value = UserPhoto.Source; }
+        }
+
+        public String UserSurname {
+            get { return NameSurname.Text; }
+            set { value = NameSurname.Text; }
+        }
+
+        public String UserDescription {
+            get { return Description.Text; }
+            set { value = Description.Text; }
+        }
+    }
+}
